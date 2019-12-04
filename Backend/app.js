461a301d69768c6e3187ef1768fa025feae4a238
srubim00 <srubim00@estudiantes.unileon.es>
2019-12-04 18:09:14 +0100

    Buscar por ingredientes, ver valoracion de pizzas.
    El usuario ya puede mirar las pizzas que ha valorado anteriormente las cuales están en su agenda, además permite buscar pizzas pro ingredientes y saber qué ingredientes contiene una pizza.

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
 const neo4j = require("neo4j-driver").v1;
-const driver = neo4j.driver("bolt://localhost",neo4j.auth.basic("neo4j", "1234")
-);
+const driver = neo4j.driver("bolt://localhost",neo4j.auth.basic("neo4j", "1234"));
+const driver2 = neo4j.driver("bolt://localhost",neo4j.auth.basic("neo4j", "1234"));
 const session = driver.session();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -65,6 +65,342 @@ app.use((req, res, next) => {
   })
 })
 
-app.listen(port, function() {
-  console.log("Example app listening on port " + 3000);
+app.post("/aleatoria", function(req,res){
+
+  var query="MATCH (n:Pizza) return n";
+  var array=[];
+  var session = driver.session();
+  var pizza;
+
+// Run a Cypher statement, reading the result in a streaming manner as records arrive:
+ session/**/
+  .run(query)
+  .subscribe({
+    onNext: function (record) {
+      //console.log(record.get(0).properties);
+      array.push(record.get(0).properties);
+    },
+    onCompleted: function () {
+      
+      var pos=Math.floor(Math.random() * array.length);
+      //res.send(array[pos]);
+      pizza=array[pos] ;  
+      console.log("PIZZA nombre inicial"+pizza.nombre);
+      query="MATCH (p:Pizzeria)-[:OFRECE]->(n:Pizza) WHERE n.nombre='"+ pizza.nombre+"' return p";
+      session
+        .run(query)
+        .subscribe({
+          onNext: function (record2) {
+            console.log(record2.get(0).properties);
+            array=[];
+            array.push(record2.get(0).properties);
+          },
+          onCompleted: function () {            
+
+            session.close();
+            pizza.pizzeria=array[0].nombre;
+            pizza.direccion=array[0].direccion;
+            pizza.telefono=array[0].telefono;console.log(pizza);
+            res.send(pizza);
+            
+          },
+          onError: function (error) {
+            console.log(error);
+          }
+        });
+      
+     },
+    onError: function (error) {
+      console.log(error);
+    }
+  });
 });
+
+
+
+  app.post("/puntuar", function(req,res){
+    var user=req.body.user;
+    var pizza=req.body.pizza;
+    var puntuacion=req.body.puntuacion;
+    var query=" START n=node(*), m=node(*) where n.usuario = '"+user+"' and m.nombre = '"+pizza+"' create (n)-[:VALORA{valoración:"+puntuacion+"}]->(m)"
+    //MATCH (n:Persona{usuario:'"+user+"'})-[v:VALORA]->(p:Pizza{nombre:'"+pizza+"'})  return v" ;
+    
+    console.log("USER="+user);
+    console.log("PIZZAAA="+pizza);
+    var array=[];
+    var session = driver.session();
+    var pizza;
+  
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+    session
+    .run(query)
+    .subscribe({
+      onNext: function (record) {
+        console.log(record.get(0).properties);
+        console.log(record.get(0));
+        console.log(record);
+        array.push(record.get(0).properties);
+      },
+      onCompleted: function () {
+        res.send({message:true});
+        
+       },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
+  });
+
+  /*var session2 = driver.session();
+  var array2=[];
+  var query2="MATCH (p:Pizzeria)-[:OFRECE]->(n:Pizza) WHERE n.nombre="+ pizza.nombre+" return p";
+  console.log(pizza.nombre);
+
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+  session2
+    .run(query2)
+    .subscribe({
+      onNext: function (record2) {
+        console.log(record2.get(0).properties);
+        array2.push(record2.get(0).properties);
+      },
+      onCompleted: function () {
+        session2.close();
+        pizza.nombre=array[0].nombre;
+        pizza.direccion=array[0].direccion;
+        pizza.telefono=array[0].telefono;
+        res.send(pizza);
+        console.log("PIZZA FINAL:"+pizza)
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });*/
+    
+//})
+
+app.post("/pizzeria", function(req,res){
+
+  var query="MATCH (p:Pizzeria)-[:OFRECE]->(n:Pizza) WHERE n.nombre="+ req.body.pizza+" return p";
+  var session = driver.session();
+  var array=[];
+// Run a Cypher statement, reading the result in a streaming manner as records arrive:
+session
+  .run(query)
+  .subscribe({
+    onNext: function (record) {
+      console.log(record.get(0).properties);
+      array.push(record.get(0).properties);
+    },
+    onCompleted: function () {
+      session.close();
+      res.send(array[0]);
+    },
+    onError: function (error) {
+      console.log(error);
+    }
+  });
+    
+})
+
+
+app.post("/mio", function(req,res){
+  var user=req.body.user;
+  console.log("USU="+user);
+  var session = driver.session();
+  var query="MATCH (n:Persona{usuario:'"+user+"'})-[v:VALORA]->(p:Pizza)<-[:OFRECE]-(e:Pizzeria) return p,v.valoración,e";
+  console.log("ENTRADO");
+  
+  var array=[];
+  var objeto;
+  
+  
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+  session
+    .run(query)
+    .subscribe({
+      onNext: function (record) {
+
+        objeto=(record.get(0).properties);
+        objeto.puntuacion=record.get(1).low;
+        objeto.pizzeria=record.get(2).properties.nombre; 
+        objeto.direccion=record.get(2).properties.direccion; 
+        objeto.telefono=record.get(2).properties.telefono;  
+        console.log(objeto);
+                                      
+        array.push(objeto);
+        
+        //console.log(array[0]);
+        //console.log(array[0].get(0).properties);
+      },
+      onCompleted: function () {
+        console.log(array[0]+"AAAA");
+        session.close();
+        res.send(array); 
+        
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
+
+});
+
+
+app.post("/ingrediente", function(req,res){
+  var user=req.body.user;
+  var ingrediente=req.body.ingrediente;
+  console.log("USU="+user);
+  var session = driver.session();
+  var query="MATCH (n:Pizzeria)-[:OFRECE]->(p:Pizza)-[:CONTIENE]->(i:Ingrediente) WHERE i.nombre='"+ingrediente+"' return p,n";
+  console.log("ENTRADO");
+  
+  var array=[];
+  var objeto;
+  
+  
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+  session
+    .run(query)
+    .subscribe({
+      onNext: function (record) {
+
+        objeto=(record.get(0).properties);
+        objeto.pizzeria=record.get(1).properties.nombre; 
+        objeto.direccion=record.get(1).properties.direccion; 
+        objeto.telefono=record.get(1).properties.telefono;  
+        console.log(objeto);
+                                      
+        array.push(objeto);
+        
+        //console.log(array[0]);
+        //console.log(array[0].get(0).properties);
+      },
+      onCompleted: function () {
+
+        session.close();
+        res.send(array); 
+        
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
+
+});
+
+app.post("/item", function(req,res){
+  var user=req.body.user;
+  var pizza=req.body.pizza;
+  console.log("USU="+user);
+  var session = driver.session();
+  var query="MATCH (p:Pizza)-[:CONTIENE]->(i:Ingrediente) WHERE p.nombre='"+pizza+"' return i";
+  console.log("ENTRADO");
+  
+  var array=[];
+  
+  
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+  session
+    .run(query)
+    .subscribe({
+      onNext: function (record) {
+
+        array.push(record.get(0).properties.nombre);
+        
+        console.log(array[0]);
+        //console.log(array[0].get(0).properties);
+      },
+      onCompleted: function () {
+
+        session.close();
+        res.send(array); 
+        
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
+
+});
+
+
+app.post("/mio2", function(req,res){
+  var user=req.body.user;
+  var objeto=req.body.objeto
+  console.log("USU="+user);
+  var query="MATCH (p:Pizzeria)-[:OFRECE]->(n:Pizza) WHERE n.nombre='"+ objeto.nombre+"' return p";
+  console.log("ENTRADO");
+  var array=[];
+  var objeto;
+  
+  
+  // Run a Cypher statement, reading the result in a streaming manner as records arrive:
+  session
+    .run(query)
+    .subscribe({
+      onNext: function (record) {
+
+        objeto=(record.get(0).properties);
+        objeto.puntuacion=record.get(1).low; 
+      
+             /*// console.log("BOI");
+             var temp=[];
+             var query2;
+             var session2 = driver2.session();
+             query2="MATCH (p:Pizzeria)-[:OFRECE]->(n:Pizza) WHERE n.nombre='"+ objeto.nombre+"' return p";
+             session2
+               .run(query2)
+               .subscribe({
+                 onNext: function (record2) {
+                   console.log(2);
+                   //console.log(record2.get(0).properties);
+                   temp=[];
+                   
+                   temp.push(record2.get(0).properties);
+                  
+                  // console.log(temp[0]);
+
+                 },
+                 onCompleted: function () {            
+       
+                   session2.close();
+                   objeto.pizzeria=temp[0].nombre;
+                   objeto.direccion=temp[0].direccion;
+                   objeto.telefono=temp[0].telefono;
+                   //console.log(1);
+                   //console.log(objeto);
+                   array.push(objeto);
+                   console.log(array[0]);
+
+                   
+                 },
+                 onError: function (error) {
+                   console.log(error);
+                 }
+               });*/
+             
+              
+        array.push(objeto);
+        
+        //console.log(array[0]);
+        //console.log(array[0].get(0).properties);
+      },
+      onCompleted: function () {
+        console.log(array[0]+"FFF");
+        res.send(array);
+      },
+      onError: function (error) {
+        console.log(error);
+      }
+    });
+});
+
+
+
+app.listen(port, function() {
+console.log("Example app listening on port " + 3000);
+});
+
+
+
